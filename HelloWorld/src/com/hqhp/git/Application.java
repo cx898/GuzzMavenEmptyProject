f95e59a79c6a624c864e1bd706aef8d572431db5
cgyue <yue1132@126.com>
2013-10-21 14:33:38 +0800

    凤飞飞、】

--- HelloWorld/src/com/hqhp/git/Application.java
+++ HelloWorld/src/com/hqhp/git/Application.java
@@ -3,7 +3,7 @@ package com.hqhp.git;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("github test...");
+		System.out.println("github test99999...");
 	}
 
 }
